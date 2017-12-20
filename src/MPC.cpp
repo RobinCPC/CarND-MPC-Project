f50d86d76af3df1c3c71b3b07314451780e14cde
robincp <chienpinchen@gmail.com>
2017-12-19 17:21:33 -0800

    Tuning parameters of MPC

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,8 +6,8 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 15;
-double dt = 0.1;
+size_t N = 20;
+double dt = 0.05;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -23,7 +23,7 @@ const double Lf = 2.67;
 
 // NOTE: feel free to play around with this
 // or do something completely different
-double ref_v = 40 * 0.5;
+double ref_v = 75;
 
 // The solver takes all the state variables and actuator
 // variables in a singular vector. Thus, we should to establish
@@ -61,20 +61,20 @@ class FG_eval {
 
     // The part of the cost based on the reference state
     for (size_t t = 0; t < N; t++) {
-      fg[0] += 3000 * CppAD::pow(vars[cte_start + t], 2);
-      fg[0] += CppAD::pow(vars[epsi_start + t], 2);
-      fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
+      fg[0] += 1.5 * CppAD::pow(vars[cte_start + t], 2);
+      fg[0] += 2.5 * CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += CppAD::pow((vars[v_start + t] - ref_v)/2, 2);
     }
 
     // Minimize the use of actuators (let turn and accerlate smoothly).
     for (size_t t = 0; t < N - 1; t++) {
-      fg[0] += CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 2000 * CppAD::pow(vars[delta_start + t], 2);
       fg[0] += CppAD::pow(vars[a_start + t], 2);
     }
 
     // Minimize the value gap between sequential actuations.
     for (size_t t = 0; t < N - 2; t++) {
-      fg[0] += 100 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += 500 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
       fg[0] += CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 
